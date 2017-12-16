7fd3c70cc36613a982403ba2423d80f5f25b6d08
Kazuho Niidome <acque.2two@gmail.com>
2017-12-16 14:42:48 +0900

    プラスが抜けていた問題を修正

--- Line Message Collect.py
+++ Line Message Collect.py
@@ -9,4 +9,4 @@ message2 =input("本日学んだこと\n ")
 message3 =input("明日やること\n ")
 message4 =input("次回までの課題\n ")
 
-print(message1+str("\n")+message2str("\n")+message3+message4)
+print(message1+str("\n")+message2+str("\n")+message3+message4)
